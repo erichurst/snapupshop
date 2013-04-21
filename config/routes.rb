f9936558742d953b835e00177775b3b7acb2af71
Eric Hurst (gideon) <eric@ovenbits.com>
2013-04-21 16:24:09 -0500

    Add SessionsController.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Snapup::Application.routes.draw do
+  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

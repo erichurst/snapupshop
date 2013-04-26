703016b9e7f616cb27beb441b202ece9c34c8b93
Eric Hurst (gideon) <eric@ovenbits.com>
2013-04-26 08:20:47 -0500

    Clean up UserTest. Move InstagramResponse method to test_helper.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,15 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "create from instagram response" do
-    response = Hashie::Mash.new(access_token: "fb2e77d.47a0479900504cb3ab4a1f626d174d2d",
-                    user: {
-                        id: "1574083",
-                        username: "snoopdogg",
-                        full_name: "Snoop Dogg",
-                        profile_picture: "http://distillery.s3.amazonaws.com/profiles/profile_1574083_75sq_1295469061.jpg"
-                    }
-                )
-    user = User.create_from_instagram_response(response)
+    user = User.create_from_instagram_response(instagram_response)
     assert_equal "1574083", user.instagram_id
     assert_equal "snoopdogg", user.instagram_username
     assert_equal "Snoop Dogg", user.instagram_full_name

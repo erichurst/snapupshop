c135a6e4948ac487c9cea5c932397b5fd23c0a76
Eric Hurst (gideon) <eric@ovenbits.com>
2013-05-02 17:22:36 -0500

    Remove encrypted_cookie_store.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Snapup::Application.config.session_store :encrypted_cookie_store, key: '_snapup_session'
+Snapup::Application.config.session_store :cookie_store, key: '_snapup_session'
